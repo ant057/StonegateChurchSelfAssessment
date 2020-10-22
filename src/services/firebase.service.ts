d246816fbf8d7056c1d20305f70efb54ff1a6a48
Anthony Cunningham <oxa547@gmail.com>
2020-10-21 19:31:28 -0500

    10.21

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -21,123 +21,125 @@ import { SelfAssessment } from 'src/models/selfassessment';
 @Injectable()
 export class FirebaseService {
 
-constructor(private afs: AngularFirestore,
-            private afstorage: AngularFireStorage) {
-}
+  constructor(private afs: AngularFirestore,
+              private afstorage: AngularFireStorage) {
+  }
 
-getSelfAssessmentQuestions(): Observable<Question[]> {
-  return this.afs.collection('/questions', ref => ref.orderBy('orderBy')).snapshotChanges().pipe(
-    map(q => {
-      const data: Question[] = [];
-      q.forEach(x => {
-        const item = x.payload.doc.data() as Question;
-        item.questionId = x.payload.doc.id;
-        data.push(item);
-      });
-      return data;
-    })
-  );
-}
+  getSelfAssessmentQuestions(): Observable<Question[]> {
+    return this.afs.collection('/questions', ref => ref.orderBy('orderBy')).snapshotChanges().pipe(
+      map(q => {
+        const data: Question[] = [];
+        q.forEach(x => {
+          const item = x.payload.doc.data() as Question;
+          item.questionId = x.payload.doc.id;
+          data.push(item);
+        });
+        return data;
+      })
+    );
+  }
 
-getSelfAssessmentSections(): Observable<Section[]> {
-  return this.afs.collection('/sections', ref => ref.orderBy('orderBy')).snapshotChanges().pipe(
-    map(q => {
-      const data: Section[] = [];
-      q.forEach(x => {
-        const item = x.payload.doc.data() as Section;
-        item.sectionId = x.payload.doc.id;
-        data.push(item);
-      });
-      return data;
-    })
-  );
-}
+  getSelfAssessmentSections(): Observable<Section[]> {
+    return this.afs.collection('/sections', ref => ref.orderBy('orderBy')).snapshotChanges().pipe(
+      map(q => {
+        const data: Section[] = [];
+        q.forEach(x => {
+          const item = x.payload.doc.data() as Section;
+          item.sectionId = x.payload.doc.id;
+          data.push(item);
+        });
+        return data;
+      })
+    );
+  }
 
-getSelfAssessments(): Observable<SelfAssessment[]> {
-  return this.afs.collection('/self-assessments', ref => ref.orderBy('createdAt')).snapshotChanges().pipe(
-    map(q => {
-      const data: SelfAssessment[] = [];
-      q.forEach(x => {
-        const item = x.payload.doc.data() as SelfAssessment;
-        item.selfAssessmentId = x.payload.doc.id;
-        data.push(item);
-      });
-      return data;
-    })
-  );
-}
+  getSelfAssessments(): Observable<SelfAssessment[]> {
+    return this.afs.collection('/self-assessments', ref => ref.orderBy('createdAt')).snapshotChanges().pipe(
+      map(q => {
+        const data: SelfAssessment[] = [];
+        q.forEach(x => {
+          const item = x.payload.doc.data() as SelfAssessment;
+          item.selfAssessmentId = x.payload.doc.id;
+          data.push(item);
+        });
+        return data;
+      })
+    );
+  }
 
-getPeerAssessments(): Observable<PeerAssessment[]> {
-  return this.afs.collection('/peer-assessments').snapshotChanges().pipe(
-    map(q => {
-      const data: PeerAssessment[] = [];
-      q.forEach(x => {
-        const item = x.payload.doc.data() as PeerAssessment;
-        item.peerAssessmentId = x.payload.doc.id;
-        data.push(item);
-      });
-      return data;
-    })
-  );
-}
+  getPeerAssessments(): Observable<PeerAssessment[]> {
+    return this.afs.collection('/peer-assessments').snapshotChanges().pipe(
+      map(q => {
+        const data: PeerAssessment[] = [];
+        q.forEach(x => {
+          const item = x.payload.doc.data() as PeerAssessment;
+          item.peerAssessmentId = x.payload.doc.id;
+          data.push(item);
+        });
+        return data;
+      })
+    );
+  }
 
-createQuestions(): void {
-}
+  // createQuestions(): void {
 
-createSelfAssessment(obj): any {
-  // atomic transaction
-  const selfAssessmentId = uuidv4();
-  const batch = this.afs.firestore.batch();
+  // }
 
-  const saRef = this.afs.collection('/self-assessments').doc(selfAssessmentId).ref;
-  batch.set(saRef, {
-    createdAt: new Date().toLocaleDateString(),
-    selfUserId: obj.userId,
-    selfUserFullName: obj.fullName,
-    questionAnswers: obj.questionAnswers,
-    contacts: obj.contacts});
+  createSelfAssessment(obj): any {
+    // atomic transaction
+    const selfAssessmentId = uuidv4();
+    const batch = this.afs.firestore.batch();
 
-  obj.contacts.forEach(element => {
-    if (element.emailAddress) {
-      const peerAssessmentId = uuidv4();
-      const peerRef = this.afs.collection('/peer-assessments').doc(peerAssessmentId).ref;
-      batch.set(peerRef, {
-        selfAssessmentId,
-        fullName: element.fullName,
-        emailAddress: element.emailAddress,
-        createdAt: new Date().toLocaleDateString(),
-        completed: false,
-        selfUserId: obj.userId,
-        linkToAssessment: this.getPeerAssessmentUrlBase() + peerAssessmentId
-      });
+    const saRef = this.afs.collection('/self-assessments').doc(selfAssessmentId).ref;
+    batch.set(saRef, {
+      createdAt: new Date().toLocaleDateString(),
+      selfUserId: obj.userId,
+      selfUserFullName: obj.fullName,
+      questionAnswers: obj.questionAnswers,
+      contacts: obj.contacts
+    });
+
+    obj.contacts.forEach(element => {
+      if (element.emailAddress) {
+        const peerAssessmentId = uuidv4();
+        const peerRef = this.afs.collection('/peer-assessments').doc(peerAssessmentId).ref;
+        batch.set(peerRef, {
+          selfAssessmentId,
+          fullName: element.fullName,
+          emailAddress: element.emailAddress,
+          createdAt: new Date().toLocaleDateString(),
+          completed: false,
+          selfUserId: obj.userId,
+          linkToAssessment: this.getPeerAssessmentUrlBase() + peerAssessmentId
+        });
+      }
+    });
+
+    return from(batch.commit());
+  }
+
+  getPeerAssessmentUrlBase(): string {
+    return !environment.production ? 'localhost:4200/peerassessment/' : 'selfassessment.firebaseapp/peerassessment/';
+  }
+
+  /*
+    getPlants() {
+      return this.afs.collection('/plants').snapshotChanges();
     }
-  });
-
-  return from(batch.commit());
-}
-
-getPeerAssessmentUrlBase(): string {
-  return !environment.production ? 'localhost:4200/peerassessment/' : 'selfassessment.firebaseapp/peerassessment/';
-}
-
-/*
-  getPlants() {
-    return this.afs.collection('/plants').snapshotChanges();
-  }
-
-  getMyPlants(uid: string) {
-    return this.afs.collection('/plants', ref => ref.where('uid' , '==', uid)).valueChanges();
-  }
-
-  uploadImage(file, filename) {
-    const storageRef = this.afstorage.ref(filename);
-    const task = storageRef.put(file);
-  }
-
-  getImageURL(filename) {
-    const storageRef = this.afstorage.ref(filename);
-    const URL = storageRef.getDownloadURL();
-    return URL;
-  }
-  */
+  
+    getMyPlants(uid: string) {
+      return this.afs.collection('/plants', ref => ref.where('uid' , '==', uid)).valueChanges();
+    }
+  
+    uploadImage(file, filename) {
+      const storageRef = this.afstorage.ref(filename);
+      const task = storageRef.put(file);
+    }
+  
+    getImageURL(filename) {
+      const storageRef = this.afstorage.ref(filename);
+      const URL = storageRef.getDownloadURL();
+      return URL;
+    }
+    */
 }
--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -1,4 +1,9 @@
+// angular
 import { Injectable } from '@angular/core';
+
+// models
+import { SelfAssessment } from '../models/selfassessment';
+import { PeerAssessment } from '../models/peerassessment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +22,146 @@ export class PDFService {
     }
   }
 
-  async generatePdf() {
+  async generatePdf(selfAssessment: SelfAssessment, peerAssessments: PeerAssessment[]) {
     await this.loadPdfMaker();
     const def = { content: 'A sample PDF document generated using Angular and PDFMake' };
-    this.pdfMake.createPdf(def).open();
+    this.pdfMake.createPdf(getDocumentDefinition(selfAssessment, peerAssessments)).open();
   }
-}
+
+  getDocumentDefinition(selfAssessment: SelfAssessment, peerAssessments: PeerAssessment[]) {
+
+    return {
+      content: [
+        {
+          text: selfAssessment.selfUserFullName,
+          style: 'header',
+          bold: true,
+          fontSize: 20
+        },
+
+        {
+          text: 'SELF-AWARENESS ASSESSMENT',
+          style: 'name',
+          bold: false,
+        },
+
+        {
+          text: 'Alright! Here are your results. Enjoy!'
+        },
+
+        {
+          text: `Quick note: With misread + and -, those are areas where you either rated yourself lower
+          than your peers (too low view of self) or higher than your peers (too high view of self).`
+        },
+
+        {
+          text: 'Gifts',
+          style: 'name'
+        },
+
+        // {
+        //   columns: [
+        //     // get the Gifts array
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 0).map(s => s.value)
+        //       ]
+        //     },
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 1).map(s => s.value)
+        //       ]
+        //     },
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 2).map(s => s.value)
+        //       ]
+        //     }
+        //   ]
+        // },
+
+        {
+          text: 'Hindrances',
+          style: 'name'
+        },
+
+        // {
+        //   columns: [
+        //     // get the Hindrances array
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 0).map(s => s.value)
+        //       ]
+        //     },
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 1).map(s => s.value)
+        //       ]
+        //     },
+        //     {
+        //       ul: [
+        //         ...this.resume.skills.filter((value, index) => index % 3 === 2).map(s => s.value)
+        //       ]
+        //     }
+        //   ]
+        // },
+
+        {
+          text: 'Education',
+          style: 'header'
+        },
+
+        this.getEducationObject(this.resume.educations),
+        {
+          text: 'Other Details',
+          style: 'header'
+        },
+        {
+          text: this.resume.otherDetails
+        },
+        {
+          text: 'Signature',
+          style: 'sign'
+        },
+        {
+          columns: [
+            { qr: this.resume.name + ', Contact No : ' + this.resume.contactNo, fit: 100 },
+            {
+              text: `(${this.resume.name})`,
+              alignment: 'right',
+            }
+          ]
+        }
+      ],
+      info: {
+        title: this.resume.name + '_RESUME',
+        author: this.resume.name,
+        subject: 'RESUME',
+        keywords: 'RESUME, ONLINE RESUME',
+      },
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 20, 0, 10]
+        },
+        name: {
+          fontSize: 16,
+          bold: true
+        },
+        jobTitle: {
+          fontSize: 14,
+          bold: true,
+          italics: true
+        },
+        sign: {
+          margin: [0, 50, 0, 10],
+          alignment: 'right',
+          italics: true
+        },
+        tableHeader: {
+          bold: true,
+        }
+      }
+    };
+  }
